c0d28b28e9001c635b8a739d50823fff455b695e
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-11 20:36:44 +0000

    Update Portfolio

---
+++ build/build.ts
@@ -0,0 +1,24 @@
+let html = await Deno.readTextFile("./source.html");
+
+const links: Record<string, string> = {
+    Deno: "https://deno.land/",
+    Kordex: "https://github.com/Kord-Extensions/kord-extensions",
+    Kotlin: "https://kotlinlang.org/",
+    TypeScript: "https://www.typescriptlang.org/",
+    TS: "https://www.typescriptlang.org/",
+    JavaScript: "https://www.javascript.com/",
+    JS: "https://www.javascript.com/",
+    Java: "https://www.java.com/",
+    Discord: "https://discord.com/",
+    QuiltMC: "https://github.com/QuiltMC",
+    Quilt: "https://github.com/QuiltMC",
+    Fabric: "https://fabricmc.net/",
+    Python: "https://www.python.org/",
+    Aleph: "https://alephjs.org"
+}
+
+Object.keys(links).forEach(element => {
+    html = html.replaceAll("[" + element + "]", `<a href="${links[element]}" class="text-success">${element}</a>`);
+});
+
+await Deno.writeTextFile("index.html", html);
